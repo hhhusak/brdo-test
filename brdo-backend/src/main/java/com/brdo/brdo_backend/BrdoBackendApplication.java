197028dd4e1d874c10eae72bf81f6d876c5a000e
Victor Husak <bmathhh95@gmail.com>
2025-06-20 22:32:11 +0300

    initial commit

---
+++ brdo-backend/src/main/java/com/brdo/brdo_backend/BrdoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brdo.brdo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrdoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrdoBackendApplication.class, args);
+	}
+
+}
---
+++ brdo-backend/src/test/java/com/brdo/brdo_backend/BrdoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brdo.brdo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrdoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
